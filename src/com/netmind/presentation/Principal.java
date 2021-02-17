0592d92c3e65656abba78de56a8838f59e108d23
romulo <angelesespinal19@gmail.com>
2021-02-17 14:56:06 +0100

    Initial commit, with calculator structure.

---
+++ src/com/netmind/presentation/Principal.java
@@ -0,0 +1,10 @@
+package com.netmind.presentation;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("this is my first java project");
+	}
+
+}
