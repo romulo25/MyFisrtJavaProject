74a54d02569f66a0d94f41f46b75ae3848ba98b7
romulo <angelesespinal19@gmail.com>
2021-02-22 11:30:02 +0100

    Student model

---
+++ src/com/netmind/model/Student.java
@@ -0,0 +1,121 @@
+package com.netmind.model;
+
+import java.util.Date;
+
+public class Student {
+
+	private Integer idStudent;
+	private String name;
+	private String surname;
+	private Integer age;
+	private Date dateOfBirth;
+
+	public Integer getIdStudent() {
+		return idStudent;
+	}
+
+	public void setIdStudent(Integer idStudent) {
+		this.idStudent = idStudent;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [idStudent=" + idStudent + ", name=" + name + ", surname=" + surname + ", age=" + age
+				+ ", dateOfBirth=" + dateOfBirth + "]";
+	}
+
+	public Student(Integer idStudent, String name, String surname, Integer age, Date dateOfBirth) {
+		super();
+		this.idStudent = idStudent;
+		this.name = name;
+		this.surname = surname;
+		this.age = age;
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public Student() {
+
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((age == null) ? 0 : age.hashCode());
+		result = prime * result + ((dateOfBirth == null) ? 0 : dateOfBirth.hashCode());
+		result = prime * result + ((idStudent == null) ? 0 : idStudent.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((surname == null) ? 0 : surname.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		if (age == null) {
+			if (other.age != null)
+				return false;
+		} else if (!age.equals(other.age))
+			return false;
+		if (dateOfBirth == null) {
+			if (other.dateOfBirth != null)
+				return false;
+		} else if (!dateOfBirth.equals(other.dateOfBirth))
+			return false;
+		if (idStudent == null) {
+			if (other.idStudent != null)
+				return false;
+		} else if (!idStudent.equals(other.idStudent))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (surname == null) {
+			if (other.surname != null)
+				return false;
+		} else if (!surname.equals(other.surname))
+			return false;
+		return true;
+	}
+
+}
