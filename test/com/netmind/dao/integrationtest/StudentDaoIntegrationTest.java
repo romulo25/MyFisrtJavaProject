fb8c477f2f48021aeefe64f0fece026c5ab1947b
romulo <angelesespinal19@gmail.com>
2021-02-27 13:07:05 +0100

    Integration test finish closes #9

--- test/com/netmind/dao/integrationtest/StudentDaoIntegrationTest.java
+++ test/com/netmind/dao/integrationtest/StudentDaoIntegrationTest.java
@@ -1,31 +1,64 @@
 package com.netmind.dao.integrationtest;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Scanner;
 import java.util.UUID;
 
 import org.junit.Test;
 
+import com.netmind.dao.FileManagerDao;
 import com.netmind.dao.StudentDao;
 import com.netmind.model.Student;
 
 public class StudentDaoIntegrationTest {
-
 	@Test
-	public void testAddStudentToFile() {
+	public void testAddStudentToFile() throws Exception {
 		Student student = new Student();
+		student.setIdStudent(1);
+		student.setName("Luis");
+		student.setSurname("Mendoza");
+		student.setAge(22);
+		student.setDateOfBirth(LocalDate.parse("1999-08-26"));
 		StudentDao studentDao = new StudentDao();
+		FileManagerDao.createFile("StudentTest.txt");
+		studentDao.addStudentToFile(student);
 
-		Student findedStudent = findStudent(student.getUUID());
-		assertEquals(student, findedStudent);
-		fail("Not yet implemented");
-
+		Student foundStudent = findStudent(student.getUUID());
+		assertEquals(student, foundStudent);
 	}
 
 	private Student findStudent(UUID uuid) throws Exception {
-		throw new Exception("the method is not yet implemented");
 
+		boolean encontrado = false;
+		File fichero = new File("StudentTest.txt");
+		String[] alumnostring = null;
+
+		try (Scanner s = new Scanner(fichero)) {
+			while (s.hasNextLine() && encontrado == false) {
+				String linea = s.nextLine();
+				alumnostring = linea.split(",");
+
+				if (alumnostring[0].equals(uuid.toString())) {
+					encontrado = true;
+				}
+			}
+		} catch (IOException e) {
+			throw e;
+		}
+
+		Student student = new Student(uuid);
+		if (encontrado == true) {
+			student.setIdStudent(Integer.parseInt(alumnostring[1].trim()));
+			student.setName(alumnostring[2].trim());
+			student.setSurname(alumnostring[3].trim());
+			student.setAge(Integer.parseInt(alumnostring[4].trim()));
+			student.setDateOfBirth(LocalDate.parse((alumnostring[5].trim())));
+		}
+
+		return student;
 	}
-
 }
